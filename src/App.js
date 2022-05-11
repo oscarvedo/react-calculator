7aac6e68b5d2e900f2d17bb8699218aa2216ec2c
oscarvedo <balnicor@gmail.com>
2022-05-11 15:18:33 -0500

    Proyecto de calculadora terminado, faltan casos específicos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Screen from './components/Screen';
+import ClearButton from './components/ClearButton';
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const addInput = val => {
+    setInput(input + val);
+  };
+
+  const calculateResult = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert('Por favor ingrese valores para realizar los calculo');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-container'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
+      <div className='calculator-container'>
+        <Screen input={input} />
+        <div className='row'>
+          <Button handleClick={addInput}>1</Button>
+          <Button handleClick={addInput}>2</Button>
+          <Button handleClick={addInput}>3</Button>
+          <Button handleClick={addInput}>+</Button>
+        </div>
+        <div className='row'>
+          <Button handleClick={addInput}>4</Button>
+          <Button handleClick={addInput}>5</Button>
+          <Button handleClick={addInput}>6</Button>
+          <Button handleClick={addInput}>-</Button>
+        </div>
+        <div className='row'>
+          <Button handleClick={addInput}>7</Button>
+          <Button handleClick={addInput}>8</Button>
+          <Button handleClick={addInput}>9</Button>
+          <Button handleClick={addInput}>*</Button>
+        </div>
+        <div className='row'>
+          <Button handleClick={calculateResult}>=</Button>
+          <Button handleClick={addInput}>0</Button>
+          <Button handleClick={addInput}>.</Button>
+          <Button handleClick={addInput}>/</Button>
+        </div>
+        <div className='row'>
+          <ClearButton handleClear={() => setInput('')}>
+            Clear
+          </ClearButton>
+        </div>
+      </div>
     </div>
   );
 }
